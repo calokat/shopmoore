0e0a60b5e970553fa51ddb6ba570bea6cc8bcf2e
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-01-16 10:51:29 -0500

    Created getStaticProps, which connects to a MongoDB database (as a test) but more importantly parses dummy.json into listings

--- pages/index.js
+++ pages/index.js
@@ -10,3 +10,32 @@ export default function Home() {
     </div>
   )
 }
+
+export async function getStaticProps() {
+  const dummyData =  fs.readFileSync(path.join(process.cwd(), "/data/dummy.json"));
+  const dummyJson = JSON.parse(dummyData.toString());
+  console.log(dummyJson);
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+ 
+// Database Name
+const dbName = 'myproject';
+ 
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  // assert.equal(null, err);
+  console.log("I am awesome");
+ 
+  const db = client.db(dbName);
+ 
+  client.close();
+});
+
+  return {
+    props:
+    {
+      dummyJson
+    }
+  } 
+}
