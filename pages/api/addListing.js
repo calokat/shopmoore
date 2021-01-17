6efd2f23f8cc2ffdbbb70d383fcdc5d8066eaa16
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-01-17 15:56:52 -0500

    addListing API endpoint now communicates with MongoDB

--- pages/api/addListing.js
+++ pages/api/addListing.js
@@ -1,3 +1,13 @@
+import {MongoClient} from 'mongodb'
+
 export default function handler(req, res) {
-  res.status(200).json({ text: req.query.price })
+    MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+        let data = JSON.parse(req.body);
+        console.log(data);
+        client.db("test").collection("listings").insertOne(data, (innerErr, result) => {
+            if (result.result.ok === 1) {
+                res.status(200).json({msg: 'Everything is going to be okay'});
+            }
+        })
+    })
 }
