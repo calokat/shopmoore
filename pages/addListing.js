de599e40617088cd7a8cf0b5e7d9a3f16d1176e7
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-01-17 17:59:58 -0500

    Transferred add listing form to add listing page

---
+++ pages/addListing.js
@@ -0,0 +1,40 @@
+function serializeFields(fields)
+{
+  let serialized = {};
+  for (let f of fields) {
+    console.log(f);
+    serialized[f.name] = f.value;
+  }
+  return serialized;
+}
+
+function toAPI(e) {
+    e.preventDefault();
+  
+    let data = serializeFields(document.querySelector("form").elements);
+    console.log(data);
+    // with help from https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+    fetch('/api/addListing', {
+      method: 'POST',
+      body: JSON.stringify(data)
+    }).then((response) => {
+      response.json().then(json => console.log(json));
+    })
+  }
+  
+
+export default function AddListing(props) {
+    return (
+        <form onSubmit={toAPI}>
+        <label htmlFor="name">Name</label>
+        <input id="name" name="name" type="text"></input><br />
+        <label htmlFor="desc">Description</label>
+        <input id="desc" name="desc" type="text"></input><br />
+        <label htmlFor="price">Price</label>
+        <input id="price" name="price" type="number"></input><br />
+        <label htmlFor="imgLink">Image URL</label>
+        <input id="imgLink" name="imgLink" type="url"></input><br />
+        <input type="submit"></input>
+      </form>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -2,30 +2,7 @@ import Head from 'next/head'
 import path from 'path'
 import Listing from '../components/listing'
 import {MongoClient} from 'mongodb'
-
-function serializeFields(fields)
-{
-  let serialized = {};
-  for (let f of fields) {
-    console.log(f);
-    serialized[f.name] = f.value;
-  }
-  return serialized;
-}
-
-function addListing(e) {
-  e.preventDefault();
-
-  let data = serializeFields(document.querySelector("form").elements);
-  console.log(data);
-  // with help from https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-  fetch('/api/addListing', {
-    method: 'POST',
-    body: JSON.stringify(data)
-  }).then((response) => {
-    response.json().then(json => console.log(json));
-  })
-}
+import Link from 'next/link'
 
 export default function Home({listings}) {
   return (
@@ -42,17 +19,7 @@ export default function Home({listings}) {
         <Listing name={listing.name} price={listing.price} imgLink={listing.imgLink} desc={listing.desc} />
         )
       })}
-      <form onSubmit={addListing}>
-        <label htmlFor="name">Name</label>
-        <input id="name" name="name" type="text"></input><br />
-        <label htmlFor="desc">Description</label>
-        <input id="desc" name="desc" type="text"></input><br />
-        <label htmlFor="price">Price</label>
-        <input id="price" name="price" type="number"></input><br />
-        <label htmlFor="imgLink">Image URL</label>
-        <input id="imgLink" name="imgLink" type="url"></input><br />
-        <input type="submit"></input>
-      </form>
+    <Link href="/addListing">Add Listing</Link>
     </div>
   )
 }
