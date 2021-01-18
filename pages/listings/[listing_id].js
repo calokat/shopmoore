1dd496689d129cd918b3dfbab2cc327587d33463
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-01-18 15:38:42 -0500

    Added error checking and listing_id validation

--- pages/listings/[listing_id].js
+++ pages/listings/[listing_id].js
@@ -1,8 +1,12 @@
 import {useRouter} from 'next/router'
+import Error from 'next/error'
 import {MongoClient} from 'mongodb'
 import Listing from '../../components/listing'
 import bson from 'bson'
-export default function ListingPage({listing}) {
+export default function ListingPage({error, listing}) {
+    if (error) {
+        return <Error statusCode={error} />
+    }
     return <Listing name={listing.name} price={listing.price} imgLink={listing.imgLink} desc={listing.desc} _id={listing._id} />
 }
 
@@ -11,19 +15,24 @@ export async function getServerSideProps(ctx) {
     const {listing_id} = ctx.query;
     let mdbClient = await MongoClient.connect(url);
     const db = mdbClient.db("test");
+    if (listing_id.length !== 24 || !bson.ObjectId.isValid(listing_id)) {
+        return {props: {
+            error: 404
+        }}
+    }
     const hashedID = bson.ObjectId.createFromHexString(listing_id);
     let potentialListing = await db.collection("listings").find({_id: hashedID}).toArray();
     let listing;
-    if (potentialListing !== [])
-    {
+    if (potentialListing.length !== 0) {
         listing = potentialListing[0];
         listing._id = listing_id;
         return {
             props: {listing}
         }
     }
-    else
-    {
-        return {props: {}}
+    else {
+        return {props: {
+            error: 404
+        }}
     }
 }
