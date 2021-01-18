ca7314be80b9b2967914a8981cd300b19056f0f0
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-01-18 15:03:48 -0500

    Created a dynamic route for listing pages

---
+++ pages/listings/[listing_id].js
@@ -0,0 +1,29 @@
+import {useRouter} from 'next/router'
+import {MongoClient} from 'mongodb'
+import Listing from '../../components/listing'
+import bson from 'bson'
+export default function ListingPage({listing}) {
+    return <Listing name={listing.name} price={listing.price} imgLink={listing.imgLink} desc={listing.desc} />
+}
+
+export async function getServerSideProps(ctx) {
+    const url = 'mongodb://localhost:27017';
+    const {listing_id} = ctx.query;
+    let mdbClient = await MongoClient.connect(url);
+    const db = mdbClient.db("test");
+    const hashedID = bson.ObjectId.createFromHexString(listing_id);
+    let potentialListing = await db.collection("listings").find({_id: hashedID}).toArray();
+    let listing;
+    if (potentialListing !== [])
+    {
+        listing = potentialListing[0];
+        listing._id = listing_id;
+        return {
+            props: {listing}
+        }
+    }
+    else
+    {
+        return {props: {}}
+    }
+}
