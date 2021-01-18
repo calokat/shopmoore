7ad72d570dddf76ac5a5ff71044f4e37712c0bcd
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-01-18 17:43:53 -0500

    Tried to implement sorting. Currently it does sort when the Sort button is pressed, only when the select input fields change after the fact

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,21 @@ import path from 'path'
 import Listing from '../components/listing'
 import {MongoClient} from 'mongodb'
 import Link from 'next/link'
+import { useState, useEffect } from 'react'
 
 export default function Home({listings}) {
+  let [sortWithVal, setSortWithVal] = useState("name");
+  let [sortByVal, setSortByVal] = useState("low-high");
+  let [sortWithPrefix, setSortWithPrefix] = useState("Alphabetically - ");
+  let [sortedListings, setSortedListings] = useState(listings);
+  useEffect(() => {
+    if (sortWithVal == "name") {
+      setSortWithPrefix("Alphabetically - ");
+    }
+    else {
+      setSortWithPrefix("");
+    }
+  });
   return (
     <div>
       <Head>
@@ -14,7 +27,31 @@ export default function Home({listings}) {
       <header>
         The Shopmoore
       </header>
-      {listings.map((listing, i) => {
+      <h1>Sort Menu</h1>
+      <select id="sortWith" onChange={(e) => {setSortWithVal(e.target.options[e.target.selectedIndex].value);}}>
+        <option value="name">Name</option>
+        <option value="price">Price</option>
+      </select>
+      <select id="sortBy" onChange={(e) => {setSortByVal(e.target.options[e.target.selectedIndex].value);}}>
+        <option value="low-high">{sortWithPrefix}Lowest to Highest</option>
+        <option value="high-low">{sortWithPrefix}Highest to Lowest</option>
+      </select>
+      <button onClick={() => {
+    setSortedListings(sortedListings.sort((l1, l2) => {
+      let [a1, a2] = [l1[sortWithVal], l2[sortWithVal]];
+      let compareModifier = (sortByVal === "low-high" ? 1 : -1);
+      if (a1 < a2) {
+        return -1 * compareModifier;
+      }
+      if (a2 < a1) {
+        return 1 * compareModifier;
+      }
+      else {
+        return 0;
+      }
+    }))
+  }}>Sort</button>
+      {sortedListings.map((listing, i) => {
         return (
         <Listing name={listing.name} price={listing.price} imgLink={listing.imgLink} desc={listing.desc} _id={listing._id} key={i} />
         )
