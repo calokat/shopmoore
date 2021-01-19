5fa42d0c418eba1b9cf9099af4c7f7eaf92d75f3
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-01-19 17:05:54 -0500

    addListing API sends back the _id and name of the added object

--- pages/api/addListing.js
+++ pages/api/addListing.js
@@ -3,10 +3,12 @@ import {MongoClient} from 'mongodb'
 export default function handler(req, res) {
     MongoClient.connect('mongodb://localhost:27017', (err, client) => {
         let data = JSON.parse(req.body);
+        let {name} = data;
         console.log(data);
         client.db("test").collection("listings").insertOne(data, (innerErr, result) => {
             if (result.result.ok === 1) {
-                res.status(200).json({msg: 'Everything is going to be okay'});
+                let _id = result.insertedId;
+                res.status(200).json({name, _id});
             }
         })
     })
