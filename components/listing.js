1df9482c07613ecc06fae2978ecd7fcb8768613d
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-01-19 14:11:52 -0500

    Image on listing is now a link

--- components/listing.js
+++ components/listing.js
@@ -6,7 +6,7 @@ export default function Listing(props) {
             <div>
                 <div className="bg-black text-white"><Link href = {`/listings/${props._id}`}>{props.name}</Link></div> 
                 <div className="border-4 border-black flex">
-                    <img src={props.imgLink} className="w-48 h-48"></img>
+                <Link href = {`/listings/${props._id}`}><img src={props.imgLink} className="w-48 h-48"></img></Link>
                     <div className="ml-16">
                         <p>Category: {props.category}</p>
                         <p>Description: {props.desc}</p>
