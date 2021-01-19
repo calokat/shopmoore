c9758b634b897879df70a90f16c58521ba57c0c7
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-01-19 18:15:17 -0500

    serializeFields and toAPI are inside AddListing. toAPI is now async, retrieves data from the API, and sends it to StatusBar

--- pages/addListing.js
+++ pages/addListing.js
@@ -1,38 +1,41 @@
 import Link from 'next/link'
 import { useState } from 'react';
+import StatusBar from '../components/statusBar'
 export default function AddListing(props) {
     
     let [statusName, setStatusName] = useState("");
     let [statusId, setStatusId] = useState("");
-function serializeFields(fields)
-{
-  let serialized = {};
-  for (let f of fields) {
-    console.log(f);
-    serialized[f.name] = f.value;
-  }
-  return serialized;
-}
+    function serializeFields(fields)
+    {
+      let serialized = {};
+      for (let f of fields) {
+        console.log(f);
+        serialized[f.name] = f.value;
+      }
+      return serialized;
+    }    
 
-function toAPI(e) {
-    e.preventDefault();
-  
-    let data = serializeFields(document.querySelector("form").elements);
-    console.log(data);
-    // with help from https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-    fetch('/api/addListing', {
-      method: 'POST',
-      body: JSON.stringify(data)
-    }).then((response) => {
-      response.json().then(json => document.querySelector("#status").innerHTML = `&#10003; Success! Your listing for ${json.name} has been created! <a href="/listings/${json._id}">View Listing</a>`);
-    })
-  }
-  
-
-export default function AddListing(props) {
+    async function toAPI(e) {
+        e.preventDefault();
+      
+        let data = serializeFields(document.querySelector("form").elements);
+        console.log(data);
+        // with help from https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+        let statusData = await fetch('/api/addListing', {
+          method: 'POST',
+          body: JSON.stringify(data)
+        }).then((response) => {
+          return response.json();
+          //.then(json => document.querySelector("#status").innerHTML = `&#10003; Success! Your listing for ${json.name} has been created! <a href="/listings/${json._id}">View Listing</a>`);
+        });
+        setStatusName(statusData.name);
+        setStatusId(statusData._id);
+      }
+    
+      
     return (
     <>
-    <div id="status"></div>
+        <StatusBar statusId={statusId} statusName={statusName}/>
         <form onSubmit={toAPI}>
             <label htmlFor="name">Name</label>
             <input id="name" name="name" type="text"></input><br />
