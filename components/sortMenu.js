30996015c9f1251d762c67d22a94cffa5c2ed13e
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-01-20 10:28:19 -0500

    Created SortMenu component

---
+++ components/sortMenu.js
@@ -0,0 +1,17 @@
+export default function SortMenu(props) {
+    return (
+        <>
+            Sort with{"    "}<select id="sortWith" onChange={props.onSortWithChange}>
+                <option value="name">Name</option>
+                <option value="price">Price</option>
+            </select>{"    "}
+            by {"    "}<select id="sortBy" onChange={props.onSortByChange}>
+                    <option value="low-high">{props.sortWithPrefix}Lowest to Highest</option>
+                    <option value="high-low">{props.sortWithPrefix}Highest to Lowest</option>
+                </select>
+            <button onClick={props.onSort}>
+                Sort
+            </button>
+        </>
+    )
+}
