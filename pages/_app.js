49a7735ce5320f666c23817db36423c252979c5c
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-01-20 09:47:18 -0500

    Added dynamic navigation to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,12 @@ import {useRouter} from 'next/router'
 import Link from 'next/link'
 function MyApp({ Component, pageProps }) {
   let nav;
-  if (useRouter().asPath !== "/") {
-    nav = (
-    <h1 className="text-3xl">
-      <Link href="/">&larr; Go to home</Link><br />
-    </h1>)
-  }
-  else {
-    nav = null;
-  }
+  let homeLink = (useRouter().asPath !== "/") ? <><Link href="/">&larr; Go to home</Link> |</> : null;
+  let addListingLink = (useRouter().asPath !== "/addListing") ? <Link href="/addListing">Add Listing</Link> : null;
+  nav = (
+    <nav className="text-3xl bg-black text-white text-center">
+      {homeLink} {addListingLink}
+    </nav>)
   return (<>
             <header className="text-5xl mt-5 ml-5">The Shopmoore</header>
             {nav}
