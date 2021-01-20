9228cb6f5976285afdb90ac067129774444fbfa7
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-01-20 12:10:53 -0500

    Made StatusBar responsive. Reorganized utility classes

--- components/statusBar.js
+++ components/statusBar.js
@@ -2,12 +2,12 @@ export default function StatusBar({statusName, statusId}) {
     let message;
     if (statusName && statusId) {
         message = (
-        <div className="bg-black text-center">
-            <span className="text-green-500">&#10003;</span> <span className="text-white">Success! Your listing for {statusName} has been created! <a href={`/listings/${statusId}`} className="underline">View Listing</a></span>
+        <div className="bg-black text-center mt-5 text-white">
+            <span className="text-green-500">&#10003;</span> <span className="text-white">Success! Your listing for {statusName} has been created! <br className="block sm:hidden"/><a href={`/listings/${statusId}`} className="underline">View Listing</a></span>
         </div>)
     }
     return (
-        <div className="bg-black text-white" id="status">
+        <div id="status">
             {message}
         </div>
     )
