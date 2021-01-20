105f20c85316584b12f14db2d1726768d395a727
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-01-20 11:36:50 -0500

    Styled nav buttons, made nav responsive

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,10 @@ import {useRouter} from 'next/router'
 import Link from 'next/link'
 function MyApp({ Component, pageProps }) {
   let nav;
-  let homeLink = (useRouter().asPath !== "/") ? <><Link href="/">&larr; Go to home</Link> |</> : null;
-  let addListingLink = (useRouter().asPath !== "/addListing") ? <Link href="/addListing">Add Listing</Link> : null;
+  let homeLink = (useRouter().asPath !== "/") ? <div className="border-2 border-white text-center"><Link href="/">&larr; Go to home</Link></div> : null;
+  let addListingLink = (useRouter().asPath !== "/addListing") ? <div className="border-2 border-white text-center"><Link href="/addListing">Add Listing</Link></div> : null;
   nav = (
-    <nav className="text-3xl bg-black text-white text-center">
+    <nav className="text-3xl bg-black text-white justify-center flex flex-col sm:flex-row sm:space-x-4 space-x-0 space-y-4 sm:space-y-0 py-1">
       {homeLink} {addListingLink}
     </nav>)
   return (<>
