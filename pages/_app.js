d954a2127db51ad323fb5064798a609c06670507
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2021-01-20 22:05:54 -0500

    Possible fix for navbar on mobile, which does not cover whole screen

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
   let homeLink = (useRouter().asPath !== "/") ? <div className="border-2 border-white text-center"><Link href="/">&larr; Go to home</Link></div> : null;
   let addListingLink = (useRouter().asPath !== "/addListing") ? <div className="border-2 border-white text-center"><Link href="/addListing">Add Listing</Link></div> : null;
   nav = (
-    <nav className="text-3xl bg-black text-white justify-center flex flex-col sm:flex-row sm:space-x-4 space-x-0 space-y-4 sm:space-y-0 py-1">
+    <nav className="text-3xl bg-black text-white justify-center flex flex-col sm:flex-row sm:space-x-4 space-x-0 space-y-4 sm:space-y-0 py-1 w-screen">
       {homeLink} {addListingLink}
     </nav>)
   return (<>
